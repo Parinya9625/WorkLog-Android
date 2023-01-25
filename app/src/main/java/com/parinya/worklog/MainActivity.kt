b56aa2ac6f906b92d9e158e04557b10842d78769
Parinya9625 <parinya9625@hotmail.com>
2023-01-25 16:09:42 +0700

    Update theme to Material You with Dynamic Color supported

--- app/src/main/java/com/parinya/worklog/MainActivity.kt
+++ app/src/main/java/com/parinya/worklog/MainActivity.kt
@@ -1,22 +1,12 @@
 package com.parinya.worklog
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.WindowCompat
-import androidx.core.view.isVisible
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.parinya.worklog.db.WorkDatabase
-import com.parinya.worklog.ui.home.HomeViewModel
-import com.parinya.worklog.ui.home.HomeViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
